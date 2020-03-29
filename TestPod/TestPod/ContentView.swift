1a2024442d90538bf062b29de43bb41a1f93e464
Carmelo Iriti <carmeloiriti@carmelos-mbp.localdomain>
2020-03-29 15:24:40 +0200

    Cocoapods works

---
+++ TestPod/TestPod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestPod
+//
+//  Created by Carmelo Iriti on 29.03.20.
+//  Copyright © 2020 Carmelo Iriti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
